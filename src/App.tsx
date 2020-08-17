e3a4a52e54689ee65b82ff0275c9ddca8e1ad0ce
Code <gsarciotto@gmail.com>
2020-08-17 09:33:34 -0300

    Created TextList

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,63 @@ import Header from "./components/Header";
 import Cta from "./components/Cta";
 //import FoodList from "./components/FoodList";
 import Cta2 from "./components/Cta2";
+import TextList from "./components/TextList";
 
 function App(): JSX.Element {
+    const cities: string[] = [
+        "Atlanta",
+        "Dallas-Fort Worth",
+        "New York City",
+        "Sacramento",
+        "Austin",
+        "Denver",
+        "Orange County",
+        "San Diego",
+        "Baltimore-Maryland",
+        "Las Vegas",
+        "Palm Springs",
+        "San Francisco Bay Area",
+        "Boston",
+        "Los Angeles",
+        "Philadelphia",
+        "Seattle",
+        "Charlotte",
+        "Miami",
+        "Phoenix",
+        "Tampha Bay",
+        "Chicago",
+        "Minneapolis - St. Paul",
+        "Portland",
+        "Washington D.C."
+    ];
+
+    const countries: string[] = [
+        "United States",
+        "Canada",
+        "Chile",
+        "Belgium",
+        "Spain",
+        "Poland",
+        "Switzerland",
+        "Ecuador",
+        "Brazil",
+        "United Kingdom",
+        "Colombia",
+        "Ireland",
+        "Mexico",
+        "Australia",
+        "New Zealand",
+        "Argentina",
+        "France",
+        "Italy",
+        "Taiwan (ROC)",
+        "Sweden",
+        "Japan",
+        "Portugal",
+        "South Africa",
+        "Guatemala"
+    ];
+
     return (
         <div>
             <Header />
@@ -14,6 +69,13 @@ function App(): JSX.Element {
                 //<FoodList title="Food Delivery in San Francisco Bay Area" />
             }
             <Cta2 />
+            <TextList
+                title="Cities Near Me"
+                additionalText="View all 500+ cities"
+                textArray={cities}
+            />
+
+            <TextList title="Countries with Uber Eats" textArray={countries} />
         </div>
     );
 }
---
+++ src/components/TextList/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./styles.css";
+
+interface TextListProps {
+    title: string;
+    additionalText?: string;
+    textArray: string[];
+}
+
+const TextList: React.FC<TextListProps> = (props: TextListProps) => {
+    return (
+        <section className="text-list-section">
+            <hr className="section-divider" />
+            <div>
+                <h2>{props.title}</h2>
+                {props.additionalText && (
+                    <a href="#" className="text-list-additional-text">
+                        {props.additionalText}
+                    </a>
+                )}
+            </div>
+            <ul>
+                {props.textArray.map((text) => (
+                    <li key={text}>
+                        <a href="#">{text}</a>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
+
+export default TextList;
