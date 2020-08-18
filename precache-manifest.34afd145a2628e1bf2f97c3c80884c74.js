df8fc627711f9a9e22f0050b9c1d3de826e867af
Code <gsarciotto@gmail.com>
2020-08-18 15:00:02 -0300

    Updates

---
+++ precache-manifest.34afd145a2628e1bf2f97c3c80884c74.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf18f87b65b6fbfc235ea96ec9a0e3f5",
+    "url": "/uber-eats-clone/index.html"
+  },
+  {
+    "revision": "b2e701a5eeacd4dcb4d0",
+    "url": "/uber-eats-clone/static/css/main.47173073.chunk.css"
+  },
+  {
+    "revision": "5428068ddbeddbf4e48d",
+    "url": "/uber-eats-clone/static/js/2.19a4c739.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uber-eats-clone/static/js/2.19a4c739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e701a5eeacd4dcb4d0",
+    "url": "/uber-eats-clone/static/js/main.8a83681d.chunk.js"
+  },
+  {
+    "revision": "5c8e0ef0de22eba496a2",
+    "url": "/uber-eats-clone/static/js/runtime-main.7c6c542a.js"
+  },
+  {
+    "revision": "cb1824db95fbcde3d0aa51b3c96e51cd",
+    "url": "/uber-eats-clone/static/media/applestore.cb1824db.svg"
+  },
+  {
+    "revision": "d38c5c61644a01dec785de7429548fbc",
+    "url": "/uber-eats-clone/static/media/arrow-left.d38c5c61.svg"
+  },
+  {
+    "revision": "5a9cc00d29c0cd8274be10793f9533b8",
+    "url": "/uber-eats-clone/static/media/arrow-right.5a9cc00d.svg"
+  },
+  {
+    "revision": "323d53fe0a611b93f0a1c6523df9f04a",
+    "url": "/uber-eats-clone/static/media/cta-background-1.323d53fe.svg"
+  },
+  {
+    "revision": "41b4cfb2b789503cd6f917d0d233aa82",
+    "url": "/uber-eats-clone/static/media/cta-background-2.41b4cfb2.svg"
+  },
+  {
+    "revision": "511f15fcf7715013fbf7186689b955c6",
+    "url": "/uber-eats-clone/static/media/cta2-1.511f15fc.svg"
+  },
+  {
+    "revision": "cb37ad14f91cc81e37a51ca4686130dd",
+    "url": "/uber-eats-clone/static/media/cta2-2.cb37ad14.svg"
+  },
+  {
+    "revision": "50855478d7209aa8183d7bafe5aab960",
+    "url": "/uber-eats-clone/static/media/cta2-3.50855478.svg"
+  },
+  {
+    "revision": "91b87188d1fdbb65b8d2532c16c456c1",
+    "url": "/uber-eats-clone/static/media/item1.91b87188.jpg"
+  },
+  {
+    "revision": "c2d83d617d241004873f4044c66a2d43",
+    "url": "/uber-eats-clone/static/media/item1.c2d83d61.jpeg"
+  },
+  {
+    "revision": "d5c20db2e8f839c1e3b4d6dd818c2800",
+    "url": "/uber-eats-clone/static/media/item1.d5c20db2.jpeg"
+  },
+  {
+    "revision": "0dbae8fee4be847c96e64351211d5553",
+    "url": "/uber-eats-clone/static/media/item2.0dbae8fe.jpg"
+  },
+  {
+    "revision": "691716fd00907450fe26b06f06c9ab24",
+    "url": "/uber-eats-clone/static/media/item2.691716fd.jpeg"
+  },
+  {
+    "revision": "c2d83d617d241004873f4044c66a2d43",
+    "url": "/uber-eats-clone/static/media/item2.c2d83d61.jpeg"
+  },
+  {
+    "revision": "691716fd00907450fe26b06f06c9ab24",
+    "url": "/uber-eats-clone/static/media/item3.691716fd.jpeg"
+  },
+  {
+    "revision": "eda16a3dfc69b2a8e3f3749efa2b0973",
+    "url": "/uber-eats-clone/static/media/item3.eda16a3d.jpg"
+  },
+  {
+    "revision": "93b4f90315b83076807a8d71616a8e89",
+    "url": "/uber-eats-clone/static/media/item4.93b4f903.jpeg"
+  },
+  {
+    "revision": "d8ae964777535f8132e70a5f16139c57",
+    "url": "/uber-eats-clone/static/media/item4.d8ae9647.jpg"
+  },
+  {
+    "revision": "20e844ea26eb075901b23e25fed4d6a6",
+    "url": "/uber-eats-clone/static/media/item5.20e844ea.jpg"
+  },
+  {
+    "revision": "db8f259dffa3c6f9eb167db36a34a884",
+    "url": "/uber-eats-clone/static/media/item6.db8f259d.jpg"
+  },
+  {
+    "revision": "df50cb797e118ac7b79e4099147d10db",
+    "url": "/uber-eats-clone/static/media/language.df50cb79.svg"
+  },
+  {
+    "revision": "da67f94d4fe9f53c4a977bb48627368c",
+    "url": "/uber-eats-clone/static/media/location.da67f94d.svg"
+  },
+  {
+    "revision": "106fefc7a98bd017c38eac02dd75bd03",
+    "url": "/uber-eats-clone/static/media/logo-facebook.106fefc7.svg"
+  },
+  {
+    "revision": "831e8fd5774bdeebd5fe403c9e7c8de7",
+    "url": "/uber-eats-clone/static/media/logo-instagram.831e8fd5.svg"
+  },
+  {
+    "revision": "27ff8f62c351492da1a6d696077dac11",
+    "url": "/uber-eats-clone/static/media/logo-twitter.27ff8f62.svg"
+  },
+  {
+    "revision": "ee037401cb5d31b23cf780808ee4ec1f",
+    "url": "/uber-eats-clone/static/media/logo.ee037401.svg"
+  }
+]);
